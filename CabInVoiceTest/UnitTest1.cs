052e8a0b29334884482889f1bd25d17a145a2c71
Ramya.U <ramyayadav2016@gmail.com>
2020-11-18 16:06:28 +0530

    [Ramya] Add . Added test case

---
+++ CabInVoiceTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+using CabInvoiceGenerator;
+using NUnit.Framework;
+
+namespace CabInVoiceTest
+{
+    public class Tests
+    {
+        //initialising instance for invoice genear
+        InVoiceGenerator invoiceGenerator; 
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Test1s this instance.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        /// TC 1.1
+        /// <summary>
+        /// Given the distance and time return total fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_ReturnTotalFare()
+        {
+            //initialising an instance
+            invoiceGenerator = new InVoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            //Act
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            //Arrange
+            double expected = 25;
+            //Assert
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}

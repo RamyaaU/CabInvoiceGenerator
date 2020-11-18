619a7d60611745f33746536721b366c82749f8a9
Ramya.U <ramyayadav2016@gmail.com>
2020-11-18 16:56:39 +0530

    [Ramya] Add . Added Ride and method to calculate total fare

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -11,6 +11,12 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Cab Invoice Generator!");
+            //initializing instance for invoice class
+            InVoiceGenerator invoiceGenerator = new InVoiceGenerator(RideType.NORMAL);
+            //calculates the fare
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+
         }
     }
 }

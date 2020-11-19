86663dae1d1f036fec1b125d76177a8ee20c3d9b
Ramya.U <ramyayadav2016@gmail.com>
2020-11-19 09:24:47 +0530

    [Ramya] Add . Added RideRepository.cs

---
+++ CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        //Dictionary to store userId and rides in the list
+        Dictionary<string, List<Ride>> userRides = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RideRepository"/> class.
+        /// </summary>
+        public RideRepository()
+        {
+            this.userRides = new Dictionary<string, List<Ride>>();
+        }
+
+        /// <summary>
+        /// Function to add ride list to specified userid.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="rides">The rides.</param>
+        /// <exception cref="CabInvoiceGenerator.CabInVoiceException">Rides are null</exception>
+        public void AddRide(string userId, Ride[] rides)
+        {
+            bool rideList = this.userRides.ContainsKey(userId);
+            try
+            {
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    this.userRides.Add(userId, list);
+                }
+            }
+            catch (CabInVoiceException)
+            {
+                throw new CabInVoiceException(CabInVoiceException.ExceptionType.NULL_RIDES, "Rides are null");
+            }
+        }
+
+        /// <summary>
+        /// Function To Get Rides List As an Array for specified UserId. 
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="CabInVoiceException">Invalid UserID</exception>
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch (Exception)
+            {
+                throw new CabInVoiceException(CabInVoiceException.ExceptionType.INVALID_USER_ID, "Invalid UserID");
+            }
+        }
+    }
+}
